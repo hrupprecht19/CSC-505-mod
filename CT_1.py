abdd34dc42f518a59cde12bd981b77c084424535
hrupprecht19 <hrupprecht81@gmail.com>
2025-12-11 20:10:59 -0700

    Start of the task list

---
+++ CT_1.py
@@ -0,0 +1,13 @@
+#The task is to create a task list for a shark week theme event.
+#Party task list manager
+#Simple task list manager tool for organizing a shark week themed event 
+
+# Main menu for the list
+def display_menu(self):
+    print("\n=== Party Task List Menu ===")
+    print("1. View all tasks")
+    print("2. Add a new task")
+    print("3. Mark a task as completed")
+    print("4. Remove a task")
+    print("5. Exit")
+
